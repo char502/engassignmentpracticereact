bce5942edb929ca01cdba249d086e8288cc4113f
char502 <char502@hotmail.com>
2021-01-03 21:11:32 +0000

    setup 1 (styling)  for react version of charity donations

--- src/App.js
+++ src/App.js
@@ -1,43 +1,207 @@
 import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import styled, { css } from 'styled-components';
+// import { v4 as uuidv4 } from 'uuid';
+
+const Container = styled.div`
+  background-color: lightsteelblue;
+  max-width: 1600px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 10px 0 30px 0;
+  border-radius: 3px;
+`;
+
+const DonationFormWrapper = styled.div`
+  background-color: whitesmoke;
+  max-width: 1400px;
+  margin: 0 auto;
+  list-style-type: none;
+  padding: 0;
+  border-radius: 3px;
+`;
+
+// const DataFromForm = styled.form`
+
+// `
+
+const FormRow = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 0.5em;
+  box-sizing: border-box;
+  & > label {
+    padding: 0.5em 1em 0.5em 1em;
+    flex: 1;
+  }
+`;
+
+const DonationGuide = styled.div`
+  text-align: center;
+  background-color: lightsteelblue;
+  width: 500px;
+  margin: 0 auto;
+  padding: 5px;
+  border-radius: 3px;
+  margin-top: 15px;
+  margin-bottom: 20px;
+  border: 2px solid grey;
+`;
+
+const DonationGuideHighlighting = styled.span`
+  font-weight: bold;
+`;
+
+const Hr = styled.hr`
+  border-top: 3px solid #bbb;
+  width: 75%;
+  padding: 1px;
+  margin: 0 auto;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  margin: 0 auto;
+  height: 25px;
+  width: 100px;
+  background-color: lightsteelblue;
+  border: none;
+  cursor: pointer;
+  border-radius: 3px;
+`;
+
+const DonationInputsResult = styled.div`
+  background-color: whitesmoke;
+  max-width: 1400px;
+  margin: 0 auto;
+  list-style-type: none;
+  padding: 0;
+  border-radius: 3px;
+  margin-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
-  const [newTodo, setNewTodo] = useState('');
-  const [todos, setTodos] = useState([]);
+  // const [newTodo, setNewTodo] = useState('');
+  // const [todos, setTodos] = useState([]);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (newTodo === '') return;
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   if (newTodo === '') return;
 
-    setTodos(prevTodos => {
-      return [...prevTodos, { id: uuidv4(), text: newTodo, completed: true }];
-    });
-    setNewTodo('');
-  };
+  //   setTodos(prevTodos => {
+  //     return [...prevTodos, { id: uuidv4(), text: newTodo, completed: true }];
+  //   });
+  //   setNewTodo('');
+  // };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          value={newTodo}
-          onChange={e => setNewTodo(e.target.value)}
-        />
+    <Container>
+      <form name='dataFromForm' id='dataFromForm'>
+        <div className='donation-title'>
+          <h2>Charity Donations</h2>
+          <h4>Please Enter the amount for donation</h4>
+        </div>
+        <DonationFormWrapper>
+          <FormRow>
+            <label htmlFor='donation--amount'>
+              Amount to Donate (£):
+              <input
+                type='number'
+                id='donation--amount'
+                name='donation--amount'
+                style={{ width: '100px' }}
+                onKeyPress='return isNumberKey(event)'
+                onInput='javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);'
+                min='1'
+                maxLength='8'
+              />
+            </label>
+          </FormRow>
+
+          <FormRow>
+            <label htmlFor='donation--type'>
+              Please Select a donation type:
+              <select name='donation--type' id='donation--type'>
+                <option hidden>Choose Donation type</option>
+                <option value='equal'>Equal</option>
+                <option value='more-odd'>More on Odd Days</option>
+              </select>
+            </label>
+          </FormRow>
+
+          <DonationGuide>
+            <h3>Guide to donation types:</h3>
+            <p>
+              <DonationGuideHighlighting>Equal: </DonationGuideHighlighting>The
+              same amount to be donated every day
+            </p>
+            <p>
+              <DonationGuideHighlighting>More-Odd: </DonationGuideHighlighting>
+              Double the amount to be donated on odd-numbered days
+            </p>
+          </DonationGuide>
+
+          <Hr />
+
+          <FormRow>
+            <Button
+              type='submit'
+              onclick='calculateResults()'
+              id='donation--button'
+              name='donation--button'
+              style={{ width: '70px' }}
+            >
+              Calculate
+            </Button>
+          </FormRow>
+        </DonationFormWrapper>
       </form>
-      <div>
-        <ul>
-          {todos.map(todos => {
-            return <li key={todos.id}>{todos.text}</li>;
-          })}
-        </ul>
-      </div>
-    </>
+
+      <DonationInputsResult style={{ paddingTop: '20px' }}>
+        {/* <label for='donation--resultAsTable'>
+          Results Table (£)
+          <div style='padding-top: 20px;'>
+            <table id='donation-inputs__resultTable'>
+              <thead>
+                <th>Amount(£)</th>
+                <th>Split Type</th>
+                <th>Day 1</th>
+                <th>Day 2</th>
+                <th>Day 3</th>
+                <th>Day 4</th>
+                <th>Day 5</th>
+                <th>Day 6</th>
+                <th>Day 7</th>
+                <th>Day 8</th>
+                <th>Day 9</th>
+                <th>Day 10</th>
+              </thead>
+
+              <tbody id='dataGoesHere'></tbody>
+            </table>
+          </div>
+        </label>
+        <div class='donation-inputs__resetButton' style='padding: 20px;'>
+          <button
+            type='reset'
+            onclick='resetCalculator()'
+            id='reset--button'
+            name='reset--button'
+            style='width: 70px;'
+          >
+            Reset
+          </button>
+        </div> */}
+      </DonationInputsResult>
+    </Container>
   );
 }
 
 export default App;
 
-// import React, { useState, useRef, useEffect } from 'react';
+/* // import React, { useState, useRef, useEffect } from 'react';
 // import TodoList from './TodoList';
 // import { v4 as uuidv4 } from 'uuid';
 
@@ -90,4 +254,4 @@ export default App;
 //   );
 // }
 
-// export default App;
+// export default App; */
