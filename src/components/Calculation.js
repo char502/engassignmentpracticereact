84c666d4119c489ac5a86424c1a4fdac377071f2
char502 <char502@hotmail.com>
2021-01-11 22:23:11 +0000

    trying to accept only certain number inputs

---
+++ src/components/Calculation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calculation = (inputValue, selectInputValue) => {
+  return (
+    <>
+      <p>This is a test</p>
+    </>
+  );
+};
+
+export default Calculation;
